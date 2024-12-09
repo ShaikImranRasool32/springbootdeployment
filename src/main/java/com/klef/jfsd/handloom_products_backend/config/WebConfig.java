281eede20342caf469f81b1dd4aa9aca652f1f57
ShaikImranRasool32 <165154059+ShaikImranRasool32@users.noreply.github.com>
2024-12-10 04:26:51 +0530

    Update WebConfig.java

--- src/main/java/com/klef/jfsd/handloom_products_backend/config/WebConfig.java
+++ src/main/java/com/klef/jfsd/handloom_products_backend/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://handloom-products.vercel.app") // Frontend URL
+                .allowedOrigins("https://handloom-products.vercel.app", "http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
