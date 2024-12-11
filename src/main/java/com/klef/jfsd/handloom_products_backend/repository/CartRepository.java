76c776375b7288d1b90c1ab1239e8fe17fa4e1ab
ShaikImranRasool32 <165154059+ShaikImranRasool32@users.noreply.github.com>
2024-12-11 18:01:15 +0530

    Update CartRepository.java

--- src/main/java/com/klef/jfsd/handloom_products_backend/repository/CartRepository.java
+++ src/main/java/com/klef/jfsd/handloom_products_backend/repository/CartRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.klef.jfsd.handloom_products_backend.model.Cart;
 
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     List<Cart> findByCustomerEmail(String customerEmail);  // Query by customer email
